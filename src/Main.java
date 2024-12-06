c5a8f8b28736b3bbc86a08fc77f8d194b94d5c5a
Otherisland <averil_tung@163.com>
2024-12-06 15:07:09 +0800

    更改

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,57 @@
 
 public class Main {
     public static void main(String[] args) {
+        //数组or字符串
+        ArrayOrStrings ex1_24 = new ArrayOrStrings();
+        //双指针
+        TwoPointers ex25_29= new TwoPointers();
+        //滑动窗口
+        SlindingWindow ex30_33= new SlindingWindow();
+        //矩阵
+        Matrix ex34_38= new Matrix();
+        //哈希表
+        HashTable ex39_47= new HashTable();
+        //区间
+        Array ex48_51= new Array();
+        //栈
+        Stack ex52_56= new Stack();
+        //链表
+        LinkedList ex57_67= new LinkedList();
+        //二叉树
+        Tree ex58_81= new Tree();
+        //二叉树层次遍历
+        BinaryTreeSearch_BreadthOrDepth ex82_85= new BinaryTreeSearch_BreadthOrDepth();
+        //二叉搜索树
+        BinaryTreeSearch ex86_88= new BinaryTreeSearch();
+        //图
+        Graph ex89_94= new Graph();
+        //图的广度优先搜索
+        BreadthFirstSearch ex90_92= new BreadthFirstSearch();
+        //字典树
+        Trie ex93_95= new Trie();
+        //回溯
+        Backtracking ex96_102=new Backtracking();
+        //分治
+        DivideAndConquer ex103_106=new DivideAndConquer();
+        //Kadane算法
+        Kadane ex107_108 = new Kadane();
+        //二分查找
+        BinarySearch ex109_115= new BinarySearch();
+        //堆
+        Heap ex116_119= new Heap();
+        //位运算
+        BitManipulation ex_120_125= new BitManipulation();
+        //数学
+        Math ex126_131= new Math();
+        //一维动态规划
+        OneDimensionalDynamicProgramming ex132_136= new OneDimensionalDynamicProgramming();
+        //多维动态规划
+        MultiDimensionalProgramming ex137_145= new MultiDimensionalProgramming();
+    }
+
+    //数组or字符串
+    static class ArrayOrStrings{
+        //数组or字符串
         //88. Merge Sorted Array
         MergeSortedArrayTest ex_88=new MergeSortedArrayTest();
         //27. Remove Element
@@ -21,7 +72,78 @@ public class Main {
         JumpGamesTest ex_55=new JumpGamesTest();
         //45. Jump Games II
         JumpGamesIITest ex_45=new JumpGamesIITest();
+        //274. H-Index
+        //H_Index ex274=new H_Index();
+        //380. Insert Delete GetRandom O(1)
 
-        //
+        //238. Product of Array Except Self
+
+        //134. Gas Station
+
+        //135. Candy
+
+        //42. Trapping Rain Water
+
+        //13. Roman to Integer
+
+        //12. Integer to Roman
+
+        //58. Length of Last Word
+
+        //14. Longest Common Prefix
+
+        //151. Reverse Words in a String
+
+        //6. Zigzag Conversion
+
+        //28. Find the Index of the First Occurrence in a String
+
+        //68. Text Justification
+
+
     }
+    //双指针
+    static class TwoPointers{ }
+    //滑动窗口
+    static class SlindingWindow{ }
+    //矩阵
+    static class Matrix{ }
+    //哈希表
+    static class HashTable{ }
+    //区间
+    static class Array{ }
+    //栈
+    static class Stack{ }
+    //链表
+    static class LinkedList{ }
+    //二叉树
+    static class Tree{ }
+    //二叉树层次遍历
+    static class BinaryTreeSearch_BreadthOrDepth{ }
+    //二叉搜索树
+    static class BinaryTreeSearch{ }
+    //图
+    static class Graph{ }
+    //图的广度优先搜索
+    static class BreadthFirstSearch{ }
+    //字典树
+    static class Trie{ }
+    //回溯
+    static class Backtracking{ }
+    //分治
+    static class DivideAndConquer{ }
+    //Kadane算法
+    static class Kadane{ }
+    //二分查找
+    static class BinarySearch{ }
+    //堆
+    static class Heap{ }
+    //位运算
+    static class BitManipulation{ }
+    //数学
+    static class Math{ }
+    //一维动态规划
+    static class OneDimensionalDynamicProgramming{ }
+    //多维动态规划
+    static class MultiDimensionalProgramming{ }
 }
