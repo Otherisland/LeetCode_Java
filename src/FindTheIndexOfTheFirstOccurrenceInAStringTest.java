9689a69a7fa314195b6fcb88ef7af8fd0e6889df
Otherisland <averil_tung@163.com>
2024-12-11 00:19:37 +0800

    更改

--- src/FindTheIndexOfTheFirstOccurrenceInAStringTest.java
+++ src/FindTheIndexOfTheFirstOccurrenceInAStringTest.java
@@ -1,2 +1,163 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class FindTheIndexOfTheFirstOccurrenceInAStringTest {
+    //方法一：自带api，方法二：暴力匹配，方法三：KMP算法
+
+    //方法一：自带api
+//    static class Solution {
+//        public int strStr(String haystack, String needle) {
+//            return haystack.indexOf(needle);
+//        }
+//    }
+
+    //方法二：暴力匹配算法,主字符串和子字符串不匹配时都要退回
+    //见ex_28.md(../handouts/ex_28.md)
+
+    //方法三：KMP算法,主串字符串不用后退，直接向前走,例如
+    //haystack:abaaaabaaabaacabaaba
+    //  needle:abaaabaaba
+    //①对子字符串needle进行前缀表初始化
+    // 见KMP.md(../handouts/KMP.md)
+    static class Solution {
+        public int strStr(String haystack, String needle) {
+            int n = haystack.length(), m = needle.length();
+            if (m == 0) {
+                return 0;
+            }
+            int[] pi = new int[m];
+            for (int i = 1, j = 0; i < m; i++) {
+                while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                    j = pi[j - 1];
+                }
+                if (needle.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                pi[i] = j;
+            }
+            for (int i = 0, j = 0; i < n; i++) {
+                while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                    j = pi[j - 1];
+                }
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                if (j == m) {
+                    return i - m + 1;
+                }
+            }
+            return -1;
+        }
+    }
+
+    // 输出输入信息
+    public void printInput(String haystack, String needle) {
+        System.out.println("输入\n");
+        System.out.println("haystack = \""+haystack+"\", needle = \""+needle+"\"");
+    }
+
+    // 输出结果和预期结果
+    public void printOutput(int result, int expected) {
+        System.out.println("输出\n" + result + "\n预期结果\n" + expected);
+    }
+
+    // 测试用例1：needle 出现在 haystack 的最开始位置
+    @Test
+    public void testCase1() {
+        Solution solution = new Solution();
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        int expect=0;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+
+    // 测试用例2：needle 不存在于 haystack 中
+    @Test
+    public void testCase2() {
+        Solution solution = new Solution();
+        String haystack = "leetcode";
+        String needle = "leeto";
+        int expect=-1;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+
+    // 测试用例3：needle 出现在 haystack 的中间位置
+    @Test
+    public void testCase3() {
+        Solution solution = new Solution();
+        String haystack = "sadbutsad";
+        String needle = "but";
+        int expect=3;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+
+    // 测试用例4：needle 和 haystack 是相同的
+    @Test
+    public void testCase4() {
+        Solution solution = new Solution();
+        String haystack = "hello";
+        String needle = "hello";
+        int expect=0;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+
+    // 测试用例5：needle 是空字符串，应该返回 0
+    @Test
+    public void testCase5() {
+        Solution solution = new Solution();
+        String haystack = "any string";
+        String needle = "";
+        int expect=0;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+
+    // 测试用例6：
+    @Test
+    public void testCase6() {
+        Solution solution = new Solution();
+        String haystack = "abaaaabaaabaacabaaca";
+        String needle = "abaaabaaca";
+        int expect=5;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
+    // 测试用例7：needle 长度大于 haystack
+    @Test
+    public void testCase7() {
+        Solution solution = new Solution();
+        String haystack = "needle";
+        String needle = "haystackaaaaa";
+        int expect=-1;
+
+        printInput(haystack, needle);
+        int result = solution.strStr(haystack, needle);
+        printOutput(expect, result);
+        assertEquals(expect, result);
+    }
 }
--- src/ZigzagConversionTest.java
+++ src/ZigzagConversionTest.java
@@ -6,33 +6,74 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ZigzagConversionTest {
+    //找规律
 
     static class Solution {
         public String convert(String s, int numRows) {
-            if(numRows<2) return s;
-            //设置numRows行的String List，从上到下填充，到头返回
-            List<StringBuilder> rows=new ArrayList<>();
-            //判断是否到头
-            int flag=-1;
-            //初始化rows
-            for(int i = 0; i < numRows; i++)
-                rows.add(new StringBuilder());
-                //若新的一行出现了，初始化
-            int i=0;flag=-1;
-            for(char c:s.toCharArray()){
-                //填充字符
-                rows.get(i).append(c);
-                //到头转向
-                if(i==0||i==numRows-1) flag=-flag;
-                //向上或者向上填充数组，通过flag控制上下
-                i+=flag;
+            if (s.length() == 1 || numRows == 1) {
+                return s;
             }
-            //结果字符串整合
-            StringBuilder res=new StringBuilder();
-            for(StringBuilder sb:rows) res.append(sb);
-            return res.toString();
+            int len = s.length();
+            char[] arr = new char[len];
+            //间隔距离，遍历中会改变
+            //例如当numRows=6时，maxInterval=10
+            //间隔为 row[0] 10,10,10,10,...
+            //      row[1]  8, 2, 8, 2,...
+            //      row[2]  6, 4, 6, 4,...
+            //      row[3]  4, 6, 4, 6,...
+            //      row[4]  2, 8, 2, 8,...
+            //      row[5] 10,10,10,10,...
+            int interval = numRows * 2 - 2;
+            //最大间隔距离
+            int maxInterval = numRows * 2 - 2;
+            int k = 0;
+            for (int i = 0; i < numRows; i++) {
+                //10,8,6,4,2,...,10
+                interval = maxInterval - 2 * i;
+                //每行第一个元素都是s.charAt[i]
+                for (int j = i; j < len;) {
+                    //填充字符
+                    arr[k++] = s.charAt(j);
+                    //到头时成为最大间隔距离
+                    if (interval == 0)
+                        interval = maxInterval;
+                    //更新下标，到达下一个位置
+                    j += interval;
+                    //计算另一个间隔距离，若为0，直接取最大间隔值，代表走到最后一行
+                    if (interval > 0)
+                        interval = maxInterval - interval;
+                }
+            }
+            return String.valueOf(arr);
         }
     }
+    //暴力遍历
+//    static class Solution {
+//        public String convert(String s, int numRows) {
+//            if(numRows<2) return s;
+//            //设置numRows行的String List，从上到下填充，到头返回
+//            List<StringBuilder> rows=new ArrayList<>();
+//            //判断是否到头
+//            int flag=-1;
+//            //初始化rows
+//            for(int i = 0; i < numRows; i++)
+//                rows.add(new StringBuilder());
+//                //若新的一行出现了，初始化
+//            int i=0;flag=-1;
+//            for(char c:s.toCharArray()){
+//                //填充字符
+//                rows.get(i).append(c);
+//                //到头转向
+//                if(i==0||i==numRows-1) flag=-flag;
+//                //向上或者向上填充数组，通过flag控制上下
+//                i+=flag;
+//            }
+//            //结果字符串整合
+//            StringBuilder res=new StringBuilder();
+//            for(StringBuilder sb:rows) res.append(sb);
+//            return res.toString();
+//        }
+//    }
     // 输出输入信息
     public void printInput(String s,int numRows) {
         System.out.println("输入");
